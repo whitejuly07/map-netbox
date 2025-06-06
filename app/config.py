e4be09e830e12c3d6b7b28688cc8b1611eb0c107
whitejuly07 <whitejuly07@gmail.com>
2025-06-06 12:07:21 +0900

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,4 +1,4 @@
 import os
 
-NETBOX_API_URL = os.getenv("NETBOX_API_URL", "https://netbox.kolmar.local/api")
-NETBOX_API_TOKEN = os.getenv("NETBOX_API_TOKEN", "015988215f1713819ba3e50ad9175f5610efee6f")
+NETBOX_API_URL = os.getenv("NETBOX_API_URL", "")
+NETBOX_API_TOKEN = os.getenv("NETBOX_API_TOKEN", "")
